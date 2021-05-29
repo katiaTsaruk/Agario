0967e231ede5f0979ae8a42c2c1da790332d73c4
Katia <katrusia.tsaruk@gmail.com>
2021-05-29 21:28:03 +0300

    start

---
+++ Agario/Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Agario
+{
+    public class Game
+    {
+        private const int windowWidth = 1280;
+        private const int windowHeight = 720;
+
+        public Game()
+        {
+            
+        }
+
+        public void Start()
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(windowWidth, windowHeight), "Game window");
+            window.Closed += WindowClosed;
+
+            while (window.IsOpen)
+            {
+                window.Clear(Color.Cyan);
+               
+                window.DispatchEvents();
+                window.Display();
+            }
+        }
+        
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            RenderWindow w = (RenderWindow)sender;
+            w.Close();
+        }
+
+        // moglo bi bit krasivo no vidimo net(((
+        // private void Background()
+        // {
+        //     Texture t=new Texture("picture");
+        //     Image image=new Image("picture.jpg");
+        //     Sprite sprite = new Sprite();
+        //     sprite.Texture = new Texture(t);
+        //     window.Draw(sprite);
+        // }
+    }
+}
---
+++ Agario/Program.cs
@@ -0,0 +1,19 @@
+﻿using SFML.Window;
+using SFML.Graphics;
+using System;
+using System.Drawing;
+using SFML.System;
+using Color = SFML.Graphics.Color;
+
+namespace Agario
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
+ 
