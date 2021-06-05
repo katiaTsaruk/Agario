2ed746062169317ce9be987a6d419a4e785a4d15
Katia <katrusia.tsaruk@gmail.com>
2021-06-05 21:18:19 +0300

    bot game

--- Agario/Circle.cs
+++ Agario/Circle.cs
@@ -9,7 +9,7 @@ namespace Agario
         private int windowWidth;
         private int windowHeight;
         public CircleShape circleShape = new CircleShape();
-        private int startRadius=5;
+        public int startRadius=4;
 
         public Circle(int windowWidth,int windowHeight)
         {
@@ -22,10 +22,21 @@ namespace Agario
             Random random=new Random();
             circleShape.Position=new Vector2f(random.Next(0,windowWidth-startRadius),random.Next(0,windowHeight-startRadius));
             circleShape.Radius = startRadius;
-            circleShape.Origin = new Vector2f(startRadius, startRadius);
+            circleShape.Origin = new Vector2f(startRadius/2, startRadius/2);
             circleShape.OutlineColor=Color.Black;
             circleShape.OutlineThickness = 1f;
-            circleShape.FillColor=Color.Yellow;
+            circleShape.FillColor=new Color((byte)random.Next(0,256),(byte)random.Next(0,256),(byte)random.Next(0,256));
         }
+        private bool TryToChangePosition(float deltaX, float deltaY)
+        {
+            float newY = circleShape.Position.Y + deltaY;
+            float newX = circleShape.Position.X + deltaX;
+            if (newX > startRadius / 2 && newX < windowWidth && newY > startRadius / 2 && newY < windowHeight)
+            {
+                return true;
+            }
+            else return false;
+        }
+        
     }
 }
--- Agario/Game.cs
+++ Agario/Game.cs
@@ -10,33 +10,104 @@ namespace Agario
     {
         private const int windowWidth = 1280;
         private const int windowHeight = 720;
+        private RenderWindow window = new RenderWindow(new VideoMode(windowWidth, windowHeight), "Game window");
         public List<Circle> AllCircles = new List<Circle>();
-        private Circle circle = new Circle(windowWidth, windowHeight);
-        private int amountOfCircles = 0;
 
-        public Game()
+        private void DrawCircles()
+        {
+            foreach(Circle circle in AllCircles)
+            {
+                window.Draw(circle.circleShape);
+            }
+        }
+        
+        private void CreateCircles()
+        {
+            while (AllCircles.Count <= 150)
+            {
+                Circle circle = new Circle(windowWidth, windowHeight);
+                AllCircles.Add(circle);
+            }
+        }
+
+        private bool IsIntersecting(int targetCircle,int currentCircle)
+        {
+            float targetX = AllCircles[targetCircle].circleShape.Position.X;
+            float currentX = AllCircles[currentCircle].circleShape.Position.X;
+            float targetY = AllCircles[targetCircle].circleShape.Position.Y;
+            float currentY = AllCircles[currentCircle].circleShape.Position.Y;
+            return targetX < currentX + AllCircles[currentCircle].circleShape.Radius &&
+                   targetX > currentX - AllCircles[currentCircle].circleShape.Radius &&
+                   targetY < currentY + AllCircles[currentCircle].circleShape.Radius &&
+                   targetY > currentY - AllCircles[currentCircle].circleShape.Radius;
+        }
+
+        private void DeleteEatedAndIncrese(int targetCircle,int currentCircle)
+        {
+            if(IsIntersecting(targetCircle, currentCircle))
+            {
+                AllCircles[currentCircle].circleShape.Radius += AllCircles[targetCircle].circleShape.Radius/10;
+                AllCircles.RemoveAt(targetCircle);
+            }
+        }
+
+        
+
+        private Vector2f FindMoveVector(int currentCircle)
+        {
+            int targetCircle = FindMoveTarget(currentCircle);
+            Vector2f moveVector = new Vector2f(
+                AllCircles[targetCircle].circleShape.Position.X - AllCircles[currentCircle].circleShape.Position.X,
+                AllCircles[targetCircle].circleShape.Position.Y - AllCircles[currentCircle].circleShape.Position.Y);
+            return moveVector;
+        }
+
+        private void BotMoveAndEat()
         {
             
+            for (int currentCircle = 0; currentCircle < 15; currentCircle++)
+            {
+                int targetCircle = FindMoveTarget(currentCircle);
+                AllCircles[currentCircle].circleShape.Position += FindMoveVector(currentCircle)/1000;
+                DeleteEatedAndIncrese(targetCircle, currentCircle);
+                
+            }
+        }
+
+        private int FindMoveTarget(int currentCircle)
+        {
+            int targetCircle=currentCircle;
+            float deltaX = AllCircles[currentCircle].circleShape.Position.X - AllCircles[currentCircle + 1].circleShape.Position.X;
+            float deltaY = AllCircles[currentCircle].circleShape.Position.Y - AllCircles[currentCircle + 1].circleShape.Position.Y;
+            float distance = deltaX * deltaX + deltaY * deltaY;
+            for (int j = 0; j < AllCircles.Count; j++)
+            {
+                if (currentCircle != j)
+                {
+                    deltaX = AllCircles[currentCircle].circleShape.Position.X - AllCircles[j].circleShape.Position.X;
+                    deltaY = AllCircles[currentCircle].circleShape.Position.Y - AllCircles[j].circleShape.Position.Y;
+                    if (distance > deltaX * deltaX + deltaY * deltaY) 
+                    {
+                        distance = deltaX * deltaX + deltaY * deltaY;
+                        targetCircle = j;
+                    }
+                }
+            }
+            return targetCircle;
         }
 
         public void Start()
         {
-            RenderWindow window = new RenderWindow(new VideoMode(windowWidth, windowHeight), "Game window");
             window.Closed += WindowClosed;
-
-            while (amountOfCircles <= 100)
-            {
-                window.Draw(circle.circleShape);
-                circle.SetCircle();
-                amountOfCircles++;
-            }
 
             while (window.IsOpen)
             {
-               window.Clear();
-
-               window.DispatchEvents();
-               window.Display();
+                window.Clear();
+                CreateCircles();
+                DrawCircles();
+                BotMoveAndEat();
+                window.DispatchEvents();
+                window.Display();
             }
         }
         
